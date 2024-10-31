df1519240f37f05f7e8633bbdeecce14304f813d
NICOLAS DE BONA <nicolas.bona@catolicasc.edu.br>
2024-10-31 19:41:52 -0300

    Reponsividade no Header

--- estudo_react/src/components/header.js
+++ estudo_react/src/components/header.js
@@ -1,26 +1,24 @@
-import logo from "../assets/Pinterest-Logo.wine.png"
+import logo from "../assets/Pinterest-Logo.wine-removebg-preview.png"
 
 export function Header(){
     return(
-        <header className="w-full h-[100vh]">
-            <div className="flex items-center justify-between ">
-                <div className="flex space-x-2">
-                    <div className="">
-                        <img src={logo} width={150} height={150}></img>
-                    </div>
-                <div className="flex space-x-6 mt-[29px]">
-                    <p className="sm:mt-2 font-semibold md:text-base" >Hoje</p>
-                    <p className="sm:mt-2 font-semibold md:text-base" >Explorar</p>
-                </div>    
+        <header className="w-full flex flex-col items-start sm:flex sm:flex-row sm:items-center justify-between">
+                <div className="flex flex-col sm:flex sm:flex-row items-center gap-4">
+                    <img src={logo} width={150} height={150} alt="Logo pinterest" />
+                    <p className="font-semibold md:text-base">Hoje</p>
+                    <p className="font-semibold md:text-base">Explorar</p>    
                 </div>
-                <div className="flex m-5 space-x-6">
-                        <p className="sm:mt-2 font-semibold md:text-base">Sobre</p>
-                        <p className="sm:mt-2 font-semibold md:text-base">Business</p>
-                        <p className="sm:mt-2 font-semibold md:text-base">Blog</p>
-                        <button className="bg-red-500 w-[100px] h-10 rounded-full text-white font-semibold">Entrar</button>
-                        <button className="bg-gray-200 w-[120px] h-10 rounded-full text-black font-semibold">Criar Conta</button>
-                </div>
-            </div>    
+                <div className="flex flex-col sm:flex sm:flex-row items-center gap-4">
+                    <p className="font-semibold md:text-base">Sobre</p>
+                    <p className="font-semibold md:text-base">Business</p>
+                    <p className="font-semibold md:text-base">Blog</p>
+                    <button className="bg-red-500 w-[100px] h-10 rounded-full text-white font-semibold">
+                        Entrar
+                    </button>
+                    <button className="bg-gray-200 w-[120px] h-10 rounded-full text-black font-semibold">
+                        Criar Conta
+                    </button>
+                 </div>    
         </header>
     );
 }
